721f427606d1890f0283773269da34de3902b80c
KO Myung-Hun <komh78@gmail.com>
2014-01-25 06:49:41 +0900

    Add a license
    For a main class, GPL v3.
    For library classes, LGPL v2.1.

--- ftpfileengine/ftpfileenginehandler.cpp
+++ ftpfileengine/ftpfileenginehandler.cpp
@@ -1,3 +1,26 @@
+/****************************************************************************
+**
+** FtpFileEngineHandler, class to register FtpFileEngine
+** Copyright (C) 2014 by KO Myung-Hun
+** All rights reserved.
+** Contact: KO Myung-Hun (komh@chollian.net)
+**
+** This file is part of K File Wizard.
+**
+** $BEGIN_LICENSE$
+**
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** $END_LICENSE$
+**
+****************************************************************************/
+
 #include <QUrl>
 
 #include "ftpfileengine.h"
--- kfilewizard.h
+++ kfilewizard.h
@@ -1,3 +1,26 @@
+/****************************************************************************
+**
+** KFileWizard, the main class of K File Wizard
+** Copyright (C) 2014 by KO Myung-Hun
+** All rights reserved.
+** Contact: KO Myung-Hun (komh@chollian.net)
+**
+** This file is part of K File Wizard.
+**
+** $BEGIN_LICENSE$
+**
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+** $END_LICENSE$
+**
+****************************************************************************/
+
 #ifndef KFILEWIZARD_H
 #define KFILEWIZARD_H
 
